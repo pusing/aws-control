106dd46ef4ce7171840a9688b625d740587eecb5
mohamed-eldafrawi <mohamed.eldafrawi@gmail.com>
2016-04-03 03:46:17 +0300

    Update aws-control.sh

--- aws-control.sh
+++ aws-control.sh
@@ -10,119 +10,163 @@
 ##    aws ec2 start-instances --instance-ids <instance_id>
 ##    aws ec2 stop-instances --instance-ids <instance_id>
 ##    aws ec2 terminate-instances --instance-ids <instance_id>
+##
 ################################################################################################################################
 
-# AWS REGIONS ARRAY
-AWS_REGIONS=("us-east-1" "us-west-1" "us-west-2" "eu-west-1"
-"eu-central-1" "ap-southeast-1" "ap-northeast-2" "ap-southeast-2"
-"sa-east-1" "us-gov-west-1" "cn-north-1")
+reset_ink='\e[0;m'
+yellow='\033[1;33m'
 
-echo "Loading..."
+red='\033[0;31m'
+light_red='\033[1;31m'
+
+green='\033[0;32m'
+light_green='\033[1;32m'
+
+blue='\033[0;34m'
+light_blue='\033[0;34m'
+
+# AWS REGIONS ARRAY
+AWS_REGIONS=( "us-east-1"
+              "us-west-1"
+              "us-west-2"
+              "eu-west-1"
+              "eu-central-1"
+              "ap-southeast-1"
+              "ap-northeast-2"
+              "ap-southeast-2"
+              "sa-east-1"
+              "us-gov-west-1"
+              "cn-north-1" )
+
+echo -e $light_green"Loading..."$reset_ink
 # Test if jq tool is installed on the system
 type jq &> /dev/null
 if [[ $? -eq 1 ]]; then
-  echo -e "Tools jq not installed...\nyou can find installation steps in https://stedolan.github.io/jq/"
+  echo -e $light_red"Tool jq not installed...\nyou can find installation steps in https://stedolan.github.io/jq/"$reset_ink
   exit
 fi
 
-echo "Locating Credentials..."
+echo -e $light_green"Locating Credentials..."$reset_ink
 # Test if there exist a credentials file and it is not empty
 if [[ ! -s ~/.aws/credentials ]] || [[ ! -e ~/.aws/credentials ]]; then
-  echo "You need to configure your AWS Credentials"
+  echo -e $light_blue"You need to configure your AWS Credentials"$reset_ink
   aws configure
 fi
 
-echo "Validating Credentials..."
+echo -e $light_green"Validating Credentials..."$reset_ink
 # Test if the aws credentials is correct
 aws ec2 describe-instances &> /dev/null
 if [[ ! $? -eq 0 ]]; then
-  echo -e "Error: Wrong AWS Credentials !!!\n\n\n"
+  echo -e $light_red"Error: Wrong AWS Credentials !!!\n\n\n"$reset_ink
   exit
 fi
 
-PS3="What do you want to do next?  "
-Option=( "Change AWS Configuration" "List Instances" "Change Default Region"
-"Start Instances" "Reboot Instances" "Stop Instances" "Terminate Instances" "Quit")
+function list_instances() {
+  echo -e $reset_ink
+  for region in "${AWS_REGIONS[@]}" ; do
+    instanceCount=`aws ec2 describe-instance-status --include-all-instances --region $region |  grep InstanceId | wc -l`
+    echo -e "Region: "$region"\t\tNumber of instances: "$instanceCount
+
+    instanceIDs=( `aws ec2 describe-instance-status --include-all-instances --region $region | jq -r '.InstanceStatuses[range(0;'$instanceCount')].InstanceId'` )
+    instanceStates=( `aws ec2 describe-instance-status --include-all-instances --region $region | jq -r '.InstanceStatuses[range(0;'$instanceCount')].InstanceState.Name'`  )
+
+    echo -e "=========================================================="
+    echo -e "Instance ID\t\tInstance State"
+    for (( i = 0; i < $instanceCount; i++ )); do
+      if [[ "${instanceStates[$i]}" == "running" ]]; then
+        echo -e $green${instanceIDs[$i]}"\t\t"${instanceStates[$i]}$reset_ink
+
+      elif [[ "${instanceStates[$i]}" == "stopped" ]]; then
+        echo -e $yellow${instanceIDs[$i]}"\t\t"${instanceStates[$i]}$reset_ink
+
+      elif [[ "${instanceStates[$i]}" == "terminated" ]]; then
+        echo -e $red${instanceIDs[$i]}"\t\t"${instanceStates[$i]}$reset_ink
+      else
+        echo -e $reset_ink${instanceIDs[$i]}"\t\t"${instanceStates[$i]}$reset_ink
+      fi
+    done
+    echo -e $reset_ink"==========================================================\n\n"
+  done
+
+}
+
+function change_def_region(){
+  echo $light_blue
+  PS3="Select a region: "
+  select def_region in ${AWS_REGIONS[@]}
+  do
+    case $def_region in
+      * ) export region=$def_region
+          echo -e $yellow
+          PS3="What do you want to do next? "
+          break
+          ;;
+    esac
+  done
+  sed -i 's/region = .*/region = '$def_region'/' ~/.aws/config
+  echo -e $yellow
+}
+
+function start_instances(){
+  echo -en $light_blue"Enter instance-ids to start: "$reset_ink
+  read instance_id
+  aws ec2 start-instances --instance-ids $instance_id 1> /dev/null
+  echo -e $yellow
+}
 
 
+function stop_instances(){
+  echo -en $light_blue"Enter instance-ids to stop: "$reset_ink
+  read instance_id
+  aws ec2 stop-instances --instance-ids $instance_id 1> /dev/null
+  echo -e $yellow
+}
+
+
+function reboot_instances(){
+  echo -en $light_blue"Enter instance-ids to reboot: "$reset_ink
+  read instance_id
+  aws ec2 reboot-instances --instance-ids $instance_id 1> /dev/null
+  echo -e $yellow
+}
+
+function terminate_instances(){
+  echo -en $light_blue"Enter instance-ids to terminate:"$reset_ink
+  read instance_id
+  aws ec2 terminate-instances --instance-ids $instance_id 1> /dev/null
+  echo -e $yellow
+}
+
+
+Option=(  "Change AWS Configuration"
+          "Change Default Region"
+          "List Instances"
+          "Start Instances"
+          "Stop Instances"
+          "Reboot Instances"
+          "Terminate Instances"
+          "Quit" )
+
+echo -e $yellow
+PS3="What do you want to do next? "
 select Option in "${Option[@]}"
 do
   case $Option in
 
-    "Change AWS Configuration" )
-    aws configure
-    ;;
+    "Change AWS Configuration" )  aws configure ;;
 
-    "List Instances" )
+    "List Instances" )  list_instances ;;
 
-    for region in "${AWS_REGIONS[@]}" ; do
+    "Change Default Region") change_def_region ;;
 
-      echo "=========================================================="
+    "Start Instances" ) start_instances ;;
 
-      echo "Region: "$region
+    "Stop Instances" )  stop_instances ;;
 
-      instanceCount=`aws ec2 describe-instance-status --include-all-instances --region $region |  grep InstanceId | wc -l`
-      echo "Number of instances: "$instanceCount
+    "Reboot Instances" ) reboot_instances ;;
 
-      instanceIDs=( `aws ec2 describe-instance-status --include-all-instances --region $region | jq -r '.InstanceStatuses[range(0;'$instanceCount')].InstanceId'` )
-      #echo ${instanceIDs[@]}
+    "Terminate Instances" ) terminate_instances ;;
 
-      instanceStates=( `aws ec2 describe-instance-status --include-all-instances --region $region | jq -r '.InstanceStatuses[range(0;'$instanceCount')].InstanceState.Name'`  )
-      #echo ${instanceStates[@]}
-
-      echo -e "Instance ID\t\tInstance State\n"
-      for (( i = 0; i < $instanceCount; i++ )); do
-        echo -e ${instanceIDs[$i]}"\t\t"${instanceStates[$i]}
-      done
-
-      echo -e "==========================================================\n\n\n"
-
-    done
-    ;;
-
-    "Change Default Region")
-
-    echo " Select a region: "
-    select defreg in ${AWS_REGIONS[@]}
-    do
-      case $defreg in
-        * ) export region=$defreg
-        break
-        ;;
-      esac
-    done
-    sed -i 's/region = .*/region = '$defreg'/' ~/.aws/config
-    ;;
-
-
-    "Start Instances" )
-    echo -n "Enter instance-ids to start: "
-    read instance_id
-    aws ec2 start-instances --instance-ids $instance_id 1> /dev/null
-    ;;
-
-    "Stop Instances" )
-    echo -n "Enter instance-ids to stop: "
-    read instance_id
-    aws ec2 stop-instances --instance-ids $instance_id 1> /dev/null
-    ;;
-
-    "Reboot Instances" )
-    echo -n "Enter instance-ids to reboot: "
-    read instance_id
-    aws ec2 reboot-instances --instance-ids $instance_id 1> /dev/null
-    ;;
-
-
-
-    "Terminate Instances" )
-    echo -n "Enter instance-ids to terminate: "
-    read instance_id
-    aws ec2 terminate-instances --instance-ids $instance_id 1> /dev/null
-    ;;
-
-    "Quit" )
-    exit;
-    ;;
+    "Quit" )  exit;;
   esac
 done
